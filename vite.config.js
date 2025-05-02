20b41c26481c485c1d8d891bef573023c17eac52
soydex <49359256+soydex@users.noreply.github.com>
2025-05-02 21:36:24 +0200

    Mise à jour des chemins d'accès dans index.html et ajout de la configuration de base dans vite.config.js pour le déploiement sur GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/React_Page/', // Chemin de base pour GitHub Pages
   plugins: [react()],
   server: {
     port: 3000, // Change port number
