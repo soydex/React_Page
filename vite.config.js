f0c936a3ca5ba73834adcdea6597c3e4e6eaf7dd
soydex <49359256+soydex@users.noreply.github.com>
2025-05-02 21:41:47 +0200

    Correction du chemin de déploiement dans package.json et mise à jour du commentaire dans vite.config.js pour clarifier le chemin de base pour GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/React_Page/', // Chemin de base pour GitHub Pages
+  base: '/React_Page/', // Chemin de base correct pour GitHub Pages
   plugins: [react()],
   server: {
     port: 3000, // Change port number
